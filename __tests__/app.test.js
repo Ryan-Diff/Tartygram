71a1f31ab19467c40dd6269652d6a8eb29f965e2
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-09-28 14:53:17 -0700

    signup w/ tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,26 @@ const fs = require('fs');
 const pool = require('../lib/utils/pool');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/user-service');
 
 describe('Tartygram routes', () => {
   beforeEach(() => {
-    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'))
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
+
+  it('signup a new user via POST', async() => {
+    const response = await request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        email: 'email@email.com',
+        password: '1234',
+        profilePhotoUrl: 'jpg.jpg'
+      });
+
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      email: 'email@email.com',
+      profilePhotoUrl: 'jpg.jpg'
+    });
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS posts CASCADE;
-DROP TABLE IF EXISTS comments CASCADE;
+-- DROP TABLE IF EXISTS posts CASCADE;
+-- DROP TABLE IF EXISTS comments CASCADE;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -9,17 +9,17 @@ CREATE TABLE users (
     profile_photo_url TEXT NOT NULL
 );
 
-CREATE TABLE posts (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES users(id),
-    photo_url TEXT NOT NULL,
-    caption TEXT NOT NULL,
-    tags TEXT[]
-);
+-- CREATE TABLE posts (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     user_id BIGINT NOT NULL REFERENCES users(id),
+--     photo_url TEXT NOT NULL,
+--     caption TEXT NOT NULL,
+--     tags TEXT[]
+-- );
 
-CREATE TABLE comments (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    comment_by TEXT NOT NULL REFERENCES users(id),
-    post_id BIGINT NOT NULL REFERENCES posts(id),
-    comment TEXT NOT NULL
-);
+-- CREATE TABLE comments (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     comment_by TEXT NOT NULL REFERENCES users(id),
+--     post_id BIGINT NOT NULL REFERENCES posts(id),
+--     comment TEXT NOT NULL
+-- );
